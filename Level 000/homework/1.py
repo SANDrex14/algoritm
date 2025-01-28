6d51b962e3cc2ae4947a6e72baad011ed717b2c8
SANDrex14 <sandrochapidze990@gmail.com>
2025-01-27 22:25:33 -0800

    commit

--- Level 000/homework/1.py
+++ Level 000/homework/1.py
@@ -1,4 +1,4 @@
-def multiple_of_c(a,b,c):
+def multiple_c(a,b,c):
     if a > b and b % c == 0:
         return (a // c) - (b // c -1)
     elif a > b and b % c != 0:
@@ -8,4 +8,4 @@ def multiple_of_c(a,b,c):
     elif a < b and a % c != 0:
         return (b // c) - (a // c)
 
-print(multiple_of_c(12,18,3))
+print(multiple_c(19,8,-3))
