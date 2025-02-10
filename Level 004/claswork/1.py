e13a43394b6562c06ef4d6328ffdc52182d7933b
SANDrex14 <sandrochapidze990@gmail.com>
2025-02-10 03:04:04 -0800

    commit

--- Level 004/claswork/1.py
+++ Level 004/claswork/1.py
@@ -1,12 +1,1 @@
-# What is a good code?
-
-# a good code takes short time and has little operations while working perfectly
-
-# what is an algorithm?
-
-# algorithm is an instruction that helps us complete a certain task. examples of an algorithm are: making a certain dish, using a navigation app to go to a certain location and so on.
-
-# a lecture is made of:
-# 1.theory
-# 2.work for the said theory
-# 3.literatures
+# no no no
